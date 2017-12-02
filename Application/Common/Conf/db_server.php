168926684cf77126956e4945e9dfdd5e3dd51991
jorkboy <allstill@live.com>
2017-12-02 10:43:06 +0800

    事隔已久，忘记了

--- Application/Common/Conf/db_server.php
+++ Application/Common/Conf/db_server.php
@@ -11,12 +11,12 @@ return [
 	'DB_HOST'               =>  '127.0.0.1', // 服务器地址
 	'DB_NAME'               =>  'blog',          // 数据库名
 	'DB_USER'               =>  'root',      // 用户名
-	'DB_PWD'                =>  'root',          // 密码
+	'DB_PWD'                =>  'all359427still',          // 密码
 	'DB_PORT'               =>  '3306',        // 端口
 	'DB_PREFIX'             =>  '',    // 数据库表前缀
 	'DB_DEBUG'  			=>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
 	'DB_FIELDS_CACHE'       =>  true,        // 启用字段缓存
-	'DB_CHARSET'            =>  'utf8',      // 数据库编码默认采用utf8
+	'DB_CHARSET'            =>  'utf8mb4',      // 数据库编码默认采用utf8
 	'DB_DEPLOY_TYPE'        =>  0, // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
 	'DB_RW_SEPARATE'        =>  false,       // 数据库读写是否分离 主从式有效
 	'DB_MASTER_NUM'         =>  1, // 读写分离后 主服务器数量
